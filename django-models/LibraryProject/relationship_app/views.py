14b86965812e0873d9e20b610f4e5e797df1239e
Oghenetejiri Enaibe <enaibs99@gmail.com>
2025-07-19 23:31:14 +0100

    roles

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Book
+
+def list_books(request):
+    books = Book.objects.all()  # <-- REQUIRED
+    return render(request, "relationship_app/list_books.html", {"books": books})  
+
+from django.views.generic.detail import DetailView
+from .models import Library
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"  # <== this exact string is required
+    context_object_name = "library"
 
 # Create your views here.
