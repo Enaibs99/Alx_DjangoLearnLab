35d0238c69b48416aa22a1164bcacd86c60e0c7e
Oghenetejiri Enaibe <enaibs99@gmail.com>
2025-07-20 17:29:56 +0100

    updated roles

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from .models import Library
