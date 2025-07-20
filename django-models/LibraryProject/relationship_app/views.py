336ab341bcddf7e38021071ea8ef54960ac1748e
Oghenetejiri Enaibe <enaibs99@gmail.com>
2025-07-20 16:42:21 +0100

    updated roles

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,50 @@
-from django.shortcuts import render
-from .models import Book
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from .models import Book, Library
+from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import user_passes_test, login_required
+from .models import UserProfile
+
+def register(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("list_books")
+    return render(request, "relationship_app/register.html", {"form": form})
 
 def list_books(request):
-    books = Book.objects.all()  # <-- REQUIRED
-    return render(request, "relationship_app/list_books.html", {"books": books})  
-
-from django.views.generic.detail import DetailView
-from .models import Library
+    context = {"books": Book.objects.all()}
+    return render(request, "relationship_app/list_books.html", context)
 
 class LibraryDetailView(DetailView):
+    template_name = "relationship_app/library_detail.html"
     model = Library
-    template_name = "relationship_app/library_detail.html"  # <== this exact string is required
-    context_object_name = "library"
 
-# Create your views here.
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+@login_required
+def admin_dashboard(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+@login_required
+def librarian_dashboard(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+@login_required
+def member_dashboard(request):
+    return render(request, 'relationship_app/member_view.html')
