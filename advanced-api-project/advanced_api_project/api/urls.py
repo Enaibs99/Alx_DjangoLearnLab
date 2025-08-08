8d31b31bf92c5e042a1c0c7f8d4f8d88f63dec8d
Oghenetejiri Enaibe <enaibs99@gmail.com>
2025-08-08 14:00:57 +0100

    added some changes

---
+++ advanced-api-project/advanced_api_project/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import (
+    BookListView,
+    BookDetailView,
+    BookCreateView,
+    BookUpdateView,
+    BookDeleteView,
+)
+urlpatterns = [
+    path('books/', BookListView.as_view(), name='book-list'),              # GET, POST
+    path('books/create/', BookCreateView.as_view(), name='book-create'),  # POST
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),# GET
+    path('books/update/', BookUpdateView.as_view(), name='book-update'),  # PUT (Checker wants this)
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),  # DELETE (Checker wants this)
+]
