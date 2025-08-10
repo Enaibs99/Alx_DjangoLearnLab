fa53562190f145bc23c1cfabe4e1a7cd528bb02e
Oghenetejiri Enaibe <enaibs99@gmail.com>
2025-08-10 20:45:23 +0100

    deleted old files

--- api_project/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from .models import Book
-from rest_framework import serializers
-from django.db import models
-
-class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = ['title', 'author']
