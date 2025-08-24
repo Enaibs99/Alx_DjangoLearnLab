638a1e4692ab219e0f2dfe23e1b084298243ad1f
Oghenetejiri Enaibe <enaibs99@gmail.com>
2025-08-24 22:04:51 +0100

    updated

--- social_media_api/posts/serializers.py
+++ social_media_api/posts/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 from .models import Post, Comment
+
+User = get_user_model()
 
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
