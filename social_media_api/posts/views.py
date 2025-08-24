1535c67cb7b6b34a46eb502952f3d89fc8fb6a59
Oghenetejiri Enaibe <enaibs99@gmail.com>
2025-08-24 20:04:27 +0100

    added CRUD operations for both posts and comments

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,26 @@
+from rest_framework import viewsets
 from django.shortcuts import render
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from rest_framework import permissions
+from .permissions import IsAuthorOrReadOnly
 
-# Create your views here.
+
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by("-created_at")
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by("-created_at")
+    serializer_class = PostSerializer
+    permission_classes =  [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+        
