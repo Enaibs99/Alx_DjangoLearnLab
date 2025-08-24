5aab0f63d7280cba7e3ba91adcf62134379f8052
Oghenetejiri Enaibe <enaibs99@gmail.com>
2025-08-24 22:39:04 +0100

    added the Post, Comment, Feed, Like and UnLike Classes

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -16,5 +16,25 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+class Like(models.Model):
+     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+     created_at = models.DateTimeField(auto_now_add=True)
+
+     class Meta:
+         unique_together = ("user", "post")
+
+     def __str__(self):
+      return self.title
+     
+         
+         
+
+    
+
+    
+    
+
+    
 
 
