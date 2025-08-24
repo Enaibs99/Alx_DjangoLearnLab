5d5ef367176f8b8addc6e2013ed0e21f8b7a0ea2
Oghenetejiri Enaibe <enaibs99@gmail.com>
2025-08-24 20:41:07 +0100

    added CRUD operations for both posts and comments

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,25 +1,34 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from django.shortcuts import render
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from rest_framework import permissions
-from .permissions import IsAuthorOrReadOnly
-
+from .permissions import IsOwnerOrReadOnly
 
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by("-created_at")
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['author']
+    search_fields = ['title', 'content']
+    ordering_fields = ['created_at', 'updated_at']
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by("-created_at")
-    serializer_class = PostSerializer
-    permission_classes =  [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    queryset = Comment.objects.all().order_by("-created_at")
+    serializer_class = CommentSerializer
+    permission_classes =  [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['post'] 
+    ordering_fields = ['created_at', 'updated_at']
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
